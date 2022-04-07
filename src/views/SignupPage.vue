577daf4718ba1d73c4813ba5e7792b224f692cea
Aurora <s_yk@naver.com>
2022-04-07 14:44:47 +0900

    feat: Create Signup Components

--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Signup</div>
+  <div>
+    <!-- 비즈니스 로직을 직접 넣는 것 지양-->
+    <SignupForm></SignupForm>
+  </div>
 </template>
 
 <script>
-export default {};
+import SignupForm from '@/components/SignupForm.vue';
+export default {
+  components: {
+    SignupForm,
+  },
+};
 </script>
 
 <style></style>
