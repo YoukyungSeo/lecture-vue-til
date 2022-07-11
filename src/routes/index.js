03e92be6a0a5511ac1bdf84a44bb427e20dbd986
YoukyungSeo <s_yk@naver.com>
2022-07-11 16:40:17 +0900

    Feat: 라우터 네비게이션 가드
    - 페이지별 인증 권한 설정(학습 노트 등록, 메인페이지, 학습 노트 수정)
    - 로그인하지 않은 사용자 접근 제어
    - 로그인 상태에 따른 로고 이동 링크 처리

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 // import LoginPage from '@/views/LoginPage.vue';
 // import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter); // 플러그인을 초기화하기 위해서 필요한 코드
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: [
     {
@@ -23,14 +24,23 @@ export default new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
       path: '*',
@@ -38,3 +48,12 @@ export default new VueRouter({
     },
   ],
 });
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    console.log('인증이 필요합니다');
+    next('/login');
+    return; // return => next 이후 또 다시 next를 하지 않기 위해
+  }
+  next();
+});
+export default router;
